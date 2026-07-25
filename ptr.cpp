2cd6167550fac99219cefda50b25d26904b9e3e7
Vishankkumar90 <vishank2345@gmail.com>
2026-07-25 19:15:19 +0530

    ok

---
+++ ptr.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int arr[6]={2,3,4,5,6,7};
+    int i;
+
+
+    while(i<6){
+        if(arr[i]!=0){
+            cout<<arr[i];
+        }
+    i++;
+
+    }
+
+ 
+    return 0;
+}
