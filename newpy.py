68272353379172006c81fb87d1af04c9653e0546
Vishankkumar90 <vishank2345@gmail.com>
2026-09-14 11:12:26 +0530

    ok

--- newpy.py
+++
@@ -1,9 +0,0 @@
-def b (a,b):
-    return (a*b/2)-4
-
-c=int(input())
-d=int(input())
-
-
-
-print()
--- np.py
+++
@@ -1,8 +0,0 @@
-import heapq
-while h:
-    d,u=heapq.heappop(h)
-    if d>dist[u]:continue
-    for v, w in g[u]:
-        if dist[v]>d+w:
-            dist[v]=d+w
-            heapq.heappush(h,(dist[v],v))
